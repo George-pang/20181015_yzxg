d308aabf82405d105c68d6a8e97e3ace8f62901d
PLQ <georgepang2016@163.com>
2018-10-18 09:57:47 +0800

    swipper effect adjustment

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ $(function () {
 
     });
 
+    //顶部导航栏滚动改变背景色
+    $(window).on("scroll", function () {
+        var scroll_Top = $(this).scrollTop();
+        if (scroll_Top > 0) {
+            $(".top-nav").css("background-color", "#2d72fc");
+        } else {
+            $(".top-nav").css("background-color", "transparent");
+        }
+    });
+
     //显示服务菜单
     $(".fw").on("mouseenter", function () {
         $(".fw-menu").css("display", "block");
@@ -15,48 +25,37 @@ $(function () {
         $(".fw-menu").css("display", "none");
     });
 
-    //顶部导航栏滚动悬浮
-    // $(window).on("scroll", function () {
-    //     var scroll_Top = $(this).scrollTop();
-    //     if (scroll_Top >= $(".top-nav").height() ) {
-    //         $(".top-nav").addClass("fix-nav");
-    //         $(".swiper-container").css("marginTop", $(".top-nav").height());
-    //     } else {
-    //         $(".top-nav").removeClass("fix-nav");
-    //         $(".swiper-container").css("marginTop", 0);
-    //     }
-    // });
 
     //提交需求弹窗
-    $("#submit-btn").on("click",function(){
-        $(".popup-wrapper").css("display","block");
+    $("#submit-btn").on("click", function () {
+        $(".popup-wrapper").css("display", "block");
     });
-    $(".submit-btn").on("click",function(){
-        $(".popup-wrapper").css("display","block");
+    $(".submit-btn").on("click", function () {
+        $(".popup-wrapper").css("display", "block");
     });
 
-    $(".sub-btn").on("click",function(){
-        var value_1=$("#username").val().trim();
-        var value_2=$("#phone").val().trim();
-        var value_3=$("#userdemand").val().trim();
-        if(value_1==""){
+    $(".sub-btn").on("click", function () {
+        var value_1 = $("#username").val().trim();
+        var value_2 = $("#phone").val().trim();
+        var value_3 = $("#userdemand").val().trim();
+        if (value_1 == "") {
             alert("请输入您的姓名！");
             return false;
-        }else if(value_2==""){
+        } else if (value_2 == "") {
             alert("请留下您的联系方式！");
             return false;
-        }else if(value_3==""){
+        } else if (value_3 == "") {
             alert("请说明您的需求！");
             return false;
         }
-        $(".popup-wrapper").css("display","none");
+        $(".popup-wrapper").css("display", "none");
         $("#username").val("");
         $("#phone").val("");
         $("#userdemand").val("");
     });
 
-    $(".close-btn").on("click",function(){
-        $(".popup-wrapper").css("display","none");
+    $(".close-btn").on("click", function () {
+        $(".popup-wrapper").css("display", "none");
         $("#username").val("");
         $("#phone").val("");
         $("#userdemand").val("");
@@ -71,7 +70,7 @@ $(function () {
         // 如果需要分页器
         pagination: {
             el: '#swiper-pagination',
-            clickable :true,
+            clickable: true,
         },
         navigation: {
             nextEl: '.swiper-button-next',
@@ -84,11 +83,11 @@ $(function () {
 
     });
     var mySwiper2 = new Swiper('#swiper2', {
-        direction: 'horizontal', 
-        loop: true, 
+        direction: 'horizontal',
+        loop: true,
         pagination: {
             el: '#case-swiper-pagination',
-            clickable :true,
+            clickable: true,
         },
         autoplay: {
             delay: 3000,
@@ -98,13 +97,13 @@ $(function () {
     });
 
     //鼠标移入轮播图暂停自动播放、显示滑块控制
-    $(".swiper-container").on("mouseenter",function(event){
-            mySwiper.autoplay.stop();
-            $(".swiper-button-next").fadeIn(1000);
-            $(".swiper-button-prev").fadeIn(1000);
-            return false;
+    $(".swiper-container").on("mouseenter", function (event) {
+        mySwiper.autoplay.stop();
+        $(".swiper-button-next").fadeIn(1000);
+        $(".swiper-button-prev").fadeIn(1000);
+        return false;
     });
-    $(".swiper-container").on("mouseleave",function(event){
+    $(".swiper-container").on("mouseleave", function (event) {
         mySwiper.autoplay.start();
         $(".swiper-button-next").fadeOut(1000);
         $(".swiper-button-prev").fadeOut(1000);
